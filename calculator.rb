5a249b57e9c1c11d41f684347fec2c49932e31c2
tuffstuffmagee <tuffstuffmagee@gmail.com>
2019-01-23 18:14:25 -0500

    reworked to be more user friendly
    Broke out into method, added end statment for invalid entry

--- calculator.rb
+++ calculator.rb
@@ -1,45 +1,36 @@
-def multiply(first_number, second_number)
-  first_number * second_number
+def math(prompt, first_number, second_number)
+  if prompt == 1
+    first_number + second_number
+  elsif prompt == 2
+    first_number - second_number
+  elsif prompt == 3
+    first_number * second_number
+  else
+    first_number / second_number
+  end
 end
-
-def divide(first_number, second_number)
-  second_number / first_number
-end
-
-def addition(first_number, second_number)
-  first_number + second_number
-end
-
-def subtract(first_number, second_number)
-  first_number - second_number
-end
-
-
-
 
 puts "What do you want to do?"
-puts "1) multiply"
-puts "2) divide"
-puts "3) add"
-puts "4) subtract"
-prompt = gets.chomp
-puts "Enter in your first number"
-first_number = gets.chomp.to_f
-puts "Enter in your second number"
-second_number = gets.chomp.to_f
-if prompt == '1'
-  puts "You have chosen to multiply #{first_number} with #{second_number}"
-  result = multiply(first_number, second_number)
-elsif prompt == '2'
-  puts "You have chosen to divide #{first_number} by #{second_number}"
-  result = divide(first_number, second_number)
-elsif prompt == '4'
-  puts "You have chosen to subtract #{second_number} from #{first_number}"
-  result = subtract(first_number, second_number)
-elsif prompt == '3'
-  puts "You have chosen to add #{first_number} and #{second_number}"
-  result = addition(first_number, second_number)
+puts "1) Add"
+puts "2) Subtract"
+puts "3) Multiply"
+puts "4) Divide"
+prompt = gets.chomp.to_f
+if prompt == 1
+  puts "You have chosen to add"
+elsif prompt == 2
+  puts "You have chosen to subtract"
+elsif prompt == 3
+  puts "You have chosen to Multiply"
+elsif prompt == 4
+  puts "You have chosen to divide"
 else
   puts "You have made an invalid choice"
 end
-puts "the answer is #{result}"
+if prompt == 1 || prompt == 2 || prompt == 3 || prompt == 4
+  puts "Enter in your first number"
+  first_number = gets.chomp.to_f
+  puts "Enter in your second number"
+  second_number = gets.chomp.to_f
+  puts "the answer is #{math(prompt, first_number, second_number).to_f}"
+end
