513d81db0395faf5a6ab7a6b788c2201c43b450d
tuffstuffmagee <tuffstuffmagee@gmail.com>
2019-01-23 11:30:35 -0500

    Program works
    Planning to condense and clean up code. move all algebra into one method that selects which operator to use dynamically.

--- calculator.rb
+++ calculator.rb
@@ -1,21 +1,45 @@
-puts "What do you want to do? 1) multiply 2) divide 3) subtract 4) find remainder"
+def multiply(first_number, second_number)
+  first_number * second_number
+end
+
+def divide(first_number, second_number)
+  second_number / first_number
+end
+
+def addition(first_number, second_number)
+  first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  first_number - second_number
+end
+
+
+
+
+puts "What do you want to do?"
+puts "1) multiply"
+puts "2) divide"
+puts "3) add"
+puts "4) subtract"
 prompt = gets.chomp
 puts "Enter in your first number"
-first_number = gets.chomp
+first_number = gets.chomp.to_f
 puts "Enter in your second number"
-second_number = gets.chomp
+second_number = gets.chomp.to_f
 if prompt == '1'
   puts "You have chosen to multiply #{first_number} with #{second_number}"
   result = multiply(first_number, second_number)
 elsif prompt == '2'
-  puts "You have chosen to divide"
+  puts "You have chosen to divide #{first_number} by #{second_number}"
   result = divide(first_number, second_number)
-elsif prompt == '3'
-  puts "You have chosen to subtract"
-  result = subtract(first_number, second_number)
 elsif prompt == '4'
-  puts "You have chosen to find the remainder"
-  result = mod(first_number, second_number)
+  puts "You have chosen to subtract #{second_number} from #{first_number}"
+  result = subtract(first_number, second_number)
+elsif prompt == '3'
+  puts "You have chosen to add #{first_number} and #{second_number}"
+  result = addition(first_number, second_number)
 else
   puts "You have made an invalid choice"
 end
+puts "the answer is #{result}"
