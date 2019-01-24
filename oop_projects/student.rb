f211b3d1289efd92f6997351ed11b307aa13a14e
tuffstuffmagee <tuffstuffmagee@gmail.com>
2019-01-24 02:40:00 -0500

    Messing around with different password encryptions

--- oop_projects/student.rb
+++ oop_projects/student.rb
@@ -16,17 +16,17 @@ class Student
      Username: #{@username}"
   end
 
-  my_password = BCrypt::Password.create(Array.new(10).map { (65 + rand(58)).chr }.join)
-    #=> "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa"
+  my_password = BCrypt::Password.create("my password")
+  => "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa"
+  puts my_password.to_s
+  puts my_password.version              #=> "2a"
+  puts my_password.cost                 #=> 10
+  puts my_password == "my password"     #=> true
+  puts my_password == "not my password" #=> false
 
-  # my_password.version              #=> "2a"
-  # my_password.cost                 #=> 10
-  # my_password == "my password"     #=> true
-  # my_password == "not my password" #=> false
-  #
-  # my_password = BCrypt::Password.new("$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa")
-  # my_password == "my password"     #=> true
-  # my_password == "not my password" #=> false
+  my_password = BCrypt::Password.new("$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa")
+  puts my_password == "my password"     #=> true
+  puts my_password == "not my password" #=> false
 
   anthony = Student.new(
     'Anthony',
@@ -42,6 +42,6 @@ class Student
     'everonesi',
     my_password
   )
-  puts anthony.password
-  puts erin
+  # puts anthony.password
+  # puts erin
 end
